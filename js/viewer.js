980cd3c90ad69e4aa006e2967e6d68f0fd3e6690
Matthew Donald Stevenson <mattdst@hotmail.com>
2018-03-02 14:58:23 -0700

    Updated with a slider

--- js/viewer.js
+++ js/viewer.js
@@ -1,15 +1,8 @@
-/*
-Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 
-Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+var slider = document.getElementById("angleSlider");
+var output = document.getElementById("display");
+output.innerHTML = slider.value;
 
-    http://aws.amazon.com/apache2.0/
-
-or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-*/
-
-/*
-
-  Set Javascript specific to the extension viewer view in this file.
-
-*/
+slider.oninput = function () {
+    output.innerText = this.value;
+}
