d33f47190c170350ee9bd3790913abb39cac0f9a
Matthew Donald Stevenson <mattdst@hotmail.com>
2018-03-05 20:24:43 -0700

    Updated sliders and add Fire Pucks button

--- js/viewer.js
+++ js/viewer.js
@@ -1,8 +1,26 @@
 
-var slider = document.getElementById("angleSlider");
-var output = document.getElementById("display");
-output.innerHTML = slider.value;
+var angleSlider = document.getElementById("angleSlider");
+var powerSlider = document.getElementById("powerSlider");
+var angleOutput = document.getElementById("angleDisplay");
+var powerOutput = document.getElementById("powerDisplay");
+var sendButton = document.getElementById("sendButton");
+var title = document.getElementById("title");
+angleOutput.innerHTML = angleSlider.value + " Degrees";
+powerOutput.innerHTML = "Power: " + powerSlider.value;
 
-slider.oninput = function () {
-    output.innerText = this.value;
+angleSlider.oninput = function () {
+    if (this.value === '1') {
+        angleOutput.innerHTML = this.value + " Degree";
+    }
+    else {
+        angleOutput.innerHTML = this.value + " Degrees";
+    }
+};
+
+powerSlider.oninput = function () {
+    powerOutput.innerHTML = "Power: " + this.value;
+}
+
+sendButton.onclick = function () {
+    title.innerHTML = "PRESSED\nAngle: " + angleSlider.value + "\nPower: " + powerSlider.value;
 }
