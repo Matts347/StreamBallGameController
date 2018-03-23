40f1e4d125780d6bb95b5dd63c801961c6a71137
Matthew Donald Stevenson <mattdst@hotmail.com>
2018-03-23 12:29:41 -0600

    Add circle slider for the angle and made changes to the power bar.

--- js/viewer.js
+++ js/viewer.js
@@ -1,60 +1,78 @@
 
-var angleSlider = document.getElementById("angleSlider");
-var powerSlider = document.getElementById("powerSlider");
-var angleOutput = document.getElementById("angleDisplay");
-var powerOutput = document.getElementById("powerDisplay");
+var leftPowerSlider = document.getElementById("leftPowerSlider");
+var rightPowerSlider = document.getElementById("rightPowerSlider");
+var leftPowerOutput = document.getElementById("leftPowerDisplay");
+var rightPowerOutput = document.getElementById("rightPowerDisplay");
 var sendButton = document.getElementById("sendButton");
 var title = document.getElementById("title");
 var leftPucks = document.getElementById("leftLauncher");
 var rightPucks = document.getElementById("rightLauncher");
-var leftOutput = document.getElementById("leftDisplay");
-var rightOutput = document.getElementById("rightDisplay");
 var puckDisplay = document.getElementById("totalPucks");
 var puckCount = 0; //this will be pulled from the player ID to know how many they actually have
 var twitchAuth;
 
 //init variables
-leftOutput.innerHTML = "Left Launcher";
-rightOutput.innerHTML = "Right Launcher";
-angleOutput.innerHTML = angleSlider.value + " Degrees";
-powerOutput.innerHTML = "Power: " + powerSlider.value;
-puckDisplay.innerHTML = "Pucks: " + puckCount;
+leftPowerOutput.innerHTML = "Power: " + leftPowerSlider.value;
+rightPowerOutput.innerHTML = "Power: " + rightPowerSlider.value;
+puckDisplay.innerHTML = "Total Pucks: " + puckCount;
 leftPucks.value = 0;
 rightPucks.value = 0;
 
-//get twitch auth values
-window.Twitch.ext.onAuthorized(function (auth) {
-    //console.log(auth.token);//debug
-    twitchAuth = auth;
-    window.Twitch.ext.listen("whisper-" + twitchAuth.userId, getUpdatedPuckCount(target, type, msg));
-    window.Twitch.ext.unlisten("whisper-" + twitchAuth.userId, getUpdatedPuckCount(target, type, msg));
+$("#rightAngle").roundSlider({
+    radius: 80,
+    circleShape: "quarter-top-left",
+    showTooltip: true,
+    value: 45,
+    min: 0,
+    max: 90,
+    tooltipFormat: "changeTooltipRight",
 });
 
+$("#leftAngle").roundSlider({
+    radius: 80,
+    circleShape: "quarter-top-right",
+    showTooltip: true,
+    value: -45,
+    min: -90,
+    max: 0,
+    tooltipFormat: "changeTooltipRight"
+});
 
-//Display for Angle Slider
-angleSlider.oninput = function () {
-    if (this.value === '1') {
-        angleOutput.innerHTML = this.value + " Degree";
-    }
-    else {
-        angleOutput.innerHTML = this.value + " Degrees";
-    }
-};
+var leftAngle = $("#leftAngle").data("roundSlider");
+var rightAngle = $("#rightAngle").data("roundSlider");
+
+function changeTooltipRight(e) {
+    var val = e.value;
+    return Math.abs(val) + '\xB0';
+}
+
+//get twitch auth values
+//window.Twitch.ext.onAuthorized(function (auth) {
+//    //console.log(auth.token);//debug
+//    twitchAuth = auth;
+//    window.Twitch.ext.listen("whisper-" + twitchAuth.userId, getUpdatedPuckCount(target, type, msg));
+//    window.Twitch.ext.unlisten("whisper-" + twitchAuth.userId, getUpdatedPuckCount(target, type, msg));
+//});
 
 //Display for Power Slider
-powerSlider.oninput = function () {
-    powerOutput.innerHTML = "Power: " + this.value;
+leftPowerSlider.oninput = function () {
+    leftPowerOutput.innerHTML = "Power: " + this.value;
+};
+rightPowerSlider.oninput = function () {
+    rightPowerOutput.innerHTML = "Power: " + this.value;
 };
 
 //Function to launch when the Fire button is pressed
 sendButton.onclick = function () {
-    var left = new Launcher('0', angleSlider.value, powerSlider.value, leftPucks.value);
-    var right = new Launcher('1', angleSlider.value, powerSlider.value, rightPucks.value);
+    //leftPowerSlider.value and pucks.value are returning strings, not int. Check with mike to see if that matters.
+    var left = new Launcher('0', Math.abs(leftAngle.option("value")), leftPowerSlider.value, leftPucks.value);
+    var right = new Launcher('1', rightAngle.option("value"), leftPowerSlider.value, rightPucks.value);
+
     var launches = [left, right];
     var launchJSON = JSON.stringify(launches);
     sendPucks(launchJSON);
     console.log("sending the following json string: " + launchJSON); //DEBUG
-    title.innerHTML = "PRESSED  Angle: " + angleSlider.value + " Power: " + powerSlider.value + " Left Pucks: " + leftPucks.value + " Right Pucks: " + rightPucks.value; //DEBUG
+    //title.innerHTML = "PRESSED  Angle: " + Math.abs(leftAngle.value) + " " + rightAngle.Value + " Power: " + leftPowerSlider.value + " Left Pucks: " + leftPucks.value + " Right Pucks: " + rightPucks.value; //DEBUG
     //maybe reset displayed values
 };
 
@@ -80,7 +98,7 @@ function sendPucks(json) {
         contentType: 'application/json',
         type: 'POST',
         headers: {
-            'x-extension-jwt': twitchAuth.token,
+            'x-extension-jwt': twitchAuth.token
         },
         data: json
     }).done(function (response) {
@@ -111,7 +129,7 @@ var Launcher = function (side, angle, power, pucks) {
     var idLength = 9;
     var generatedId = "";
     var charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  
+
     for (var i = 0; i < idLength; i++) {
         generatedId += charSet.charAt(Math.floor(Math.random() * charSet.length));
     }
@@ -124,4 +142,6 @@ var Launcher = function (side, angle, power, pucks) {
         "pucks": pucks,
         "timestamp": Date.now()
     };
-}
+};
+
+
