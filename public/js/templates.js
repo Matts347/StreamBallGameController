064985ab94f9813cef820e789c8218cfa6d72a9d
Michael Curtis <contact@streamgenie.tv>
2018-04-27 16:04:27 -0600

    progress on stylesheets

--- public/js/templates.js
+++ public/js/templates.js
@@ -7,14 +7,14 @@ templates['header'] = template({"compiler":[7,">= 4.0.0"],"main":function(contai
     + alias2(alias1((depth0 != null ? depth0.avatarUrl : depth0), depth0))
     + "\" />\r\n</div>\r\n<div id=\"userNameContainer\">\r\n    <h1><span id=\"userName\">"
     + alias2(alias1((depth0 != null ? depth0.userName : depth0), depth0))
-    + "</span></h1>\r\n    <h3><span id=\"totalPucks\">Pucks: "
+    + "</span></h1>\r\n    <h3>Pucks: <span id=\"totalPucks\">"
     + alias2(alias1((depth0 != null ? depth0.totalPucks : depth0), depth0))
-    + "</span></h3>\r\n    <h3><span id=\"playerScore\">Score: "
+    + "</span></h3>\r\n    <h3>Score: <span id=\"playerScore\">"
     + alias2(alias1((depth0 != null ? depth0.playerScore : depth0), depth0))
     + "</span></h3>\r\n</div>";
 },"useData":true});
 templates['launch'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div class=\"powerContainer\">\r\n    <input type=\"range\" min=\"1\" max=\"100\" value=\"50\" class=\"leftPowerSlider\" id=\"leftPowerSlider\">\r\n    <input type=\"range\" min=\"1\" max=\"100\" value=\"50\" class=\"rightPowerSlider\" id=\"rightPowerSlider\">\r\n</div>\r\n<div class=\"powerDisplay\">\r\n    <p><span id=\"leftPowerDisplay\"></span></p>\r\n    <p><span id=\"rightPowerDisplay\"></span></p>\r\n</div>\r\n<div class=\"slider\">\r\n    <input id=\"leftAngle\" />\r\n    <input id=\"rightAngle\" />\r\n</div>\r\n<div class=\"puckNumber\">\r\n    <input type=\"text\" id=\"leftLauncher\" class=\"leftLauncher\" onkeypress=\"return AllowNumbersOnly(event)\">\r\n    <input type=\"text\" id=\"rightLauncher\" class=\"rightLauncher\" onkeypress=\"return AllowNumbersOnly(event)\">\r\n</div>\r\n<div class=\"sendButton\">\r\n    <button type=\"button\" class=\"block\" id=\"sendButton\">Fire Pucks</button>\r\n</div>";
+    return "<div class=\"slider\">\r\n    <input id=\"leftAngle\" />\r\n    <input id=\"rightAngle\" />\r\n</div>\r\n<div class=\"powerContainer\">\r\n    <input type=\"range\" min=\"1\" max=\"100\" value=\"50\" class=\"leftPowerSlider\" id=\"leftPowerSlider\">\r\n    <input type=\"range\" min=\"1\" max=\"100\" value=\"50\" class=\"rightPowerSlider\" id=\"rightPowerSlider\">\r\n</div>\r\n<div class=\"powerDisplay\">\r\n    <p><span id=\"leftPowerDisplay\"></span></p>\r\n    <p><span id=\"rightPowerDisplay\"></span></p>\r\n</div>\r\n<div class=\"puckNumber\">\r\n    <input type=\"text\" id=\"leftLauncher\" class=\"leftLauncher\" onkeypress=\"return AllowNumbersOnly(event)\">\r\n    <input type=\"text\" id=\"rightLauncher\" class=\"rightLauncher\" onkeypress=\"return AllowNumbersOnly(event)\">\r\n</div>\r\n<div class=\"sendButton\">\r\n    <button type=\"button\" class=\"block\" id=\"sendButton\">Fire Pucks</button>\r\n</div>";
 },"useData":true});
 templates['loading'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     return "<p>Loading...</p>";
