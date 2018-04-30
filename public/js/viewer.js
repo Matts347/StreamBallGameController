def1816f3dc4efc69a9ba974a36becd67536e06e
Michael Curtis <contact@streamgenie.tv>
2018-04-30 16:41:38 -0600

    adding more styling

--- public/js/viewer.js
+++ public/js/viewer.js
@@ -1,3 +1,11 @@
+/*
+License for roundSlider
+Copyright (c) 2015-2018, Soundar
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: 
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. 
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+*/
+
 var twitchAuth;
 var userInfo;
 var newUser = true;
@@ -39,6 +47,7 @@ function getUserInfo(trueUserId) {
         }
     }).done(function (response) {
         LoadHeaderTemplate(response.display_name, response.logo, undefined, undefined);
+        userInfo = response;
         console.log(response); //debug
         }).fail(function () {
             console.log("getUserInfo failed");
